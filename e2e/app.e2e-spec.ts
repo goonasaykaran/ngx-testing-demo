9dff52443038658e7cfd1e8e0fe73566e10da45e
Zackary Chapple <zackarychapple30@gmail.com>
2016-10-18 23:19:10 -0400

    feat(tests):initial test working

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,14 @@ describe('e2e-demo App', function() {
     page = new E2eDemoPage();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('should display message saying app works', async () => {
+    await page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
+  });
+
+  it('should navigate to hero when clicked', async () => {
+    await page.navigateTo();
+    await page.getHero();
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,21 @@
-import { browser, element, by } from 'protractor';
-
 export class E2eDemoPage {
-  navigateTo() {
-    return browser.get('/');
+  sleep(time: number = 500) {
+    return browser.sleep(time)
+  }
+
+  async navigateTo() {
+    browser.get('/');
+    return this.sleep(1000);
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('my-app > h1')).getText();
   }
+
+  async getHero() {
+    this.sleep(1750);
+    await browser.driver.findElements(by.id('Narco'));
+    return element(by.id('Narco')).click();
+  }
+
 }
