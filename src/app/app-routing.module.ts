b0ca8d9eb9b3f019839e7b28fc7b81504c543c48
Zackary Chapple <zackarychapple30@gmail.com>
2016-10-18 22:41:01 -0400

    feature(build out): initial commit of tour of heroes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: []
+  exports: [RouterModule]
 })
-export class E2eDemoRoutingModule { }
+export class AppRoutingModule { }
+
+export const routedComponents = [DashboardComponent, HeroesComponent, HeroDetailComponent];
--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+document.addEventListener('DOMContentLoaded', function () {
+  platformBrowserDynamic().bootstrapModule(AppModule, []);
+}, false);
