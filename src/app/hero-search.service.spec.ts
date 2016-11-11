afbe24c5e0c2a8c8c079704503a0362cc377576e
Zackary Chapple <zackarychapple30@gmail.com>
2016-11-11 11:20:12 -0500

    chore(tests):formatting html

--- src/app/hero-search.service.spec.ts
+++ src/app/hero-search.service.spec.ts
@@ -1,16 +1,50 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
 import { HeroSearchService } from './hero-search.service';
+import { TestBed, inject } from '@angular/core/testing';
+import { BaseRequestOptions, Http, RequestMethod, Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Hero } from './hero';
+
+let MockHero: Hero = <Hero>{id: 1, name: 'Superman'};
+let MockHero2: Hero = <Hero>{id: 2, name: 'IronMan'};
+let MockHeroesArray: Array<Hero> = [ MockHero, MockHero2 ];
 
 describe('Service: HeroSearch', () => {
+  let mockBackend: MockBackend;
+  let heroSearchService: HeroSearchService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HeroSearchService]
+      providers: [
+        HeroSearchService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+          deps: [ MockBackend, BaseRequestOptions ]
+        }
+      ]
     });
   });
 
-  it('should ...', inject([HeroSearchService], (service: HeroSearchService) => {
+  beforeEach(inject([ MockBackend, Http ],
+    (mb: MockBackend, http: Http) => {
+      mockBackend = mb;
+      heroSearchService = new HeroSearchService(http);
+    }));
+  it('should ...', inject([ HeroSearchService ], (service: HeroSearchService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should return observable with hero array when search is called with term', (done) => {
+    let searchTerm = 'some term';
+    mockBackend.connections.subscribe((connection: MockConnection) => {
+      expect(connection.request.method).toEqual(RequestMethod.Get);
+      expect(connection.request.url).toEqual(`app/heroes/?name=${searchTerm}`);
+      connection.mockRespond(new Response(new ResponseOptions({body: {data: MockHeroesArray}})));
+    });
+    heroSearchService.search(searchTerm).subscribe(result => {
+      expect(result).toEqual(MockHeroesArray);
+      done();
+    });
+  });
 });
