743354c466081cefe869207c4e7eac43fe5cda6f
Zackary Chapple <zackarychapple30@gmail.com>
2016-11-11 10:32:06 -0500

    chore(tests):formatting html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
 import { InMemoryDataService } from './in-memory-data.service';
-
 import './rxjs-extensions';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routedComponents } from './app-routing.module';
@@ -19,7 +17,7 @@ import { HeroSearchService } from './hero-search.service';
     FormsModule,
     AppRoutingModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 600 })
+    InMemoryWebApiModule.forRoot(InMemoryDataService, {delay: 600})
   ],
   declarations: [
     AppComponent,
@@ -30,6 +28,7 @@ import { HeroSearchService } from './hero-search.service';
     HeroService,
     HeroSearchService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
