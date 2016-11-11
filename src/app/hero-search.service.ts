1f0235c185bb6b2472764f4ae49028565127c0d6
Zackary Chapple <zackarychapple30@gmail.com>
2016-11-11 11:21:15 -0500

    chore(tests):formatting html

--- src/app/hero-search.service.ts
+++ src/app/hero-search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Hero } from './hero';
+import './rxjs-extensions';
 
 @Injectable()
 export class HeroSearchService {
@@ -11,6 +12,8 @@ export class HeroSearchService {
   search(term: string): Observable<Hero[]> {
     return this.http
       .get(`app/heroes/?name=${term}`)
-      .map((r: Response) => r.json().data as Hero[]);
+      .map((r: Response) => {
+        return r.json().data as Hero[]
+      });
   }
 }
