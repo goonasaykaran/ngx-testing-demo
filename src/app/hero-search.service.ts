11d2a9c4247245939d1de3aab801a6a8801bf458
Zackary Chapple <zackarychapple30@gmail.com>
2016-11-11 11:22:42 -0500

    chore(tests):formatting html

--- src/app/hero-search.service.ts
+++ src/app/hero-search.service.ts
@@ -12,8 +12,6 @@ export class HeroSearchService {
   search(term: string): Observable<Hero[]> {
     return this.http
       .get(`app/heroes/?name=${term}`)
-      .map((r: Response) => {
-        return r.json().data as Hero[]
-      });
+      .map((r: Response) => r.json().data as Hero[]);
   }
 }
