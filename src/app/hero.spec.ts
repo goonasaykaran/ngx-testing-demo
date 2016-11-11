0f2b96617f9ef1a293f998829410e03256ca7eba
Zackary Chapple <zackarychapple30@gmail.com>
2016-11-11 09:48:49 -0500

    chore(tests):adding tests

--- src/app/hero.spec.ts
+++ src/app/hero.spec.ts
@@ -1,10 +1,8 @@
-/* tslint:disable:no-unused-variable */
-
-import { addProviders, async, inject } from '@angular/core/testing';
-import {Hero} from './hero';
+import { Hero } from './hero';
 
 describe('Hero', () => {
   it('should create an instance', () => {
-    expect(new Hero()).toBeTruthy();
+    let hero = new Hero();
+    expect(hero).toBeTruthy();
   });
 });
